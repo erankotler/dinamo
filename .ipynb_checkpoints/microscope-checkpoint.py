6c752546f4df7de725ddda7e7e7f3c152f9b2d6b
erankotler <erankotler@gmail.com>
2023-10-10 01:05:56 -0700

    Working microscope v0

--- .ipynb_checkpoints/microscope-checkpoint.py
+++ .ipynb_checkpoints/microscope-checkpoint.py
@@ -70,7 +70,7 @@ class Dataset():
         # Rename targets (labels) of data to 0/1 instead of case/control, non-responders/responders etc.
 
         # Get samples for which we have a label:
-        samps_with_data = self.mat.columns
+        samps_with_data = self.df.index
         samps_with_label = self.groups.index
         samps_with_both = [s for s in samps_with_data if s in samps_with_label]       
         # print(len(samps_with_label), len(samps_with_both)) # ***Debugging
@@ -93,6 +93,10 @@ class Dataset():
     def organize_data(self):
         # Organize input data for sklearn usage
         self.df = self.mat.transpose() # mat is transposed (rows=CpGs, cols=Pats) -> .df is corrected
+        # Shuffle df rows
+        self.df = self.df.sample(frac=1, random_state=42)
+
+        
         self.update_features()
         self.update_samples()
         self.add_target_lables() # Add y labels to self.y slot (0/1)
@@ -127,20 +131,17 @@ def train_on_entire_dataset(Dataset,
     if nan_policy is not None:
         imp_vals = feature_imputation_values(df, nan_policy=nan_policy)
         df = pd.DataFrame(data=numba_fillna(df.values, imp_vals.values), index=df.index, columns=df.columns) # Fast fillna() with imputed values       
-        # df = df.fillna(imp_vals)
     else:
         imp_vals=None
     
-    # Feature selection    
-    if feat_selection=="wilcox":  
-        print("Selecting features using Mann-whitney")
-        mw_ps = stats.mannwhitneyu(df.iloc[np.where(y==0)[0], :], df.iloc[np.where(y==1)[0], :])[1]
-        feats_used = list(compress(df.columns, np.where(mw_ps<feat_selection_pval_thresh)[0]))
-        X = df.loc[:,feats_used].values
+    # Feature selection   
+    if feat_selection is not None:
+        feats_used = select_features(df, y, by=feat_selection, pval_thresh=feat_selection_pval_thresh)
     else:
-        print("no feature selection applied")
         feats_used = list(df.columns)
-        X = df.values
+        
+    print("Retained %i features"%len(feats_used))
+    X = df.loc[:,feats_used].values
         
     model, scaler = train_model(X, y, penalty=penalty, internalCV_folds=internalCV_folds)
 
@@ -156,6 +157,22 @@ def train_on_entire_dataset(Dataset,
         
     return (outputs)
 
+
+def select_features(df, y, by="wilcox", pval_thresh=0.05):
+    if by=="wilcox":
+        print('Selecting features using wilcoxon')
+        p_vals = stats.mannwhitneyu(df.iloc[np.where(y==0)[0], :], df.iloc[np.where(y==1)[0], :])[1]
+        feats_to_keep = list(compress(df.columns, np.where(p_vals<pval_thresh)[0]))
+    elif by=="ttest":
+        print('Selecting features using wilcoxon')
+        p_vals = stats.ttest_ind(df.iloc[np.where(y==0)[0], :], df.iloc[np.where(y==1)[0], :])[1]
+        feats_to_keep = list(compress(df.columns, np.where(p_vals<pval_thresh)[0]))
+    else:
+        print("no feature selection applied")
+        feats_to_keep = list(df.columns)
+    return(feats_to_keep)
+    
+
 def cv_train_test(Dataset, 
                   CV = 5, # "LOO" # 10 #"LOO"
                   penalty = 'l1', 
@@ -165,10 +182,9 @@ def cv_train_test(Dataset,
                   nan_policy="impute_by_median",
                   out_f=None):
     """ Perform CV training and evaluation on DNAm Dataset object """
+    
     cv = LeaveOneOut() if CV=="LOO" else KFold(n_splits=CV)
-    X_tests, y_tests = [], []
-    models, y_preds, pred_probs = [], [], []
-    feats_used = [] # features selected in each fold
+    X_tests, y_tests, models, scalers, feats_used, y_preds, pred_probs = [], [], [], [], [], [], []
 
     print ("Starting cross validation")
     for fold, (train_index, test_index) in enumerate(cv.split(Dataset.df)):
@@ -177,12 +193,18 @@ def cv_train_test(Dataset,
         start = timeit.default_timer()
         # split df to train test (instead of np.array) for wilcoxon comparions (can be optimized for speed later)
         df_train, df_test = Dataset.df.iloc[train_index], Dataset.df.iloc[test_index] 
-        y_train, y_test = Dataset.y[train_index], Dataset.y[test_index]
+        y_train, y_test = Dataset.y.iloc[train_index], Dataset.y.iloc[test_index]
+        
+        # print("=========")
+        # print(train_index[:5], test_index[:5])
+        # print(df_train.iloc[:5,:5], "\n", y_train.iloc[:5], "\n---\n", df_test.iloc[:5,:5], y_test.iloc[:5])
+        # print("=========")
+        
         # print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
         print("CV fold", fold, "Train size: %i, test size: %i (fract positives in train: %.3f)"%(df_train.shape[0], df_test.shape[0], y_train.mean()))
         
         # remove features that are NaN in entire train set:  ###TODO- remove features above certain fraction of NaNs in train data
-        df_train = df_train.dropna(how="all", axis=1)
+        df_train = df_train.dropna(how="any", axis=1)
         df_test = df_test.loc[:, df_train.columns]
 
         if nan_policy is not None: # Impute missing values in train set
@@ -196,25 +218,16 @@ def cv_train_test(Dataset,
         stop2 = timeit.default_timer()
         print('Starting feature selection, elapsed time: %.1f sec'%(stop2 - start))
         
-        # Feature selection by wilcooxn (on train set):
-        if feat_selection=="wilcox":
-            print('Starting feature selection using wilcoxon')
-            p_vals = stats.mannwhitneyu(df_train.iloc[np.where(y_train==0)[0], :], df_train.iloc[np.where(y_train==1)[0], :])[1]
-            feats_to_keep = list(compress(df_train.columns, np.where(p_vals<feat_selection_pval_thresh)[0]))
-            X_train = df_train.loc[:,feats_to_keep].values
-            X_test = df_test.loc[:, feats_to_keep].values 
-        elif feat_selection=="ttest":
-            print('Starting feature selection using t-test')
-            p_vals = stats.ttest_ind(df_train.iloc[np.where(y_train==0)[0], :], df_train.iloc[np.where(y_train==1)[0], :])[1]
-            feats_to_keep = list(compress(df_train.columns, np.where(p_vals<feat_selection_pval_thresh)[0]))
-            X_train = df_train.loc[:,feats_to_keep].values
-            X_test = df_test.loc[:, feats_to_keep].values 
+        # Feature selection (based on train set contrasting)
+        if feat_selection is not None:
+            feats_to_keep = select_features(df_train, y_train, by=feat_selection, pval_thresh=feat_selection_pval_thresh)
         else:
-            print("no feature selection applied")
             feats_to_keep = list(df_train.columns)
-            X_train = df_train.values
-            X_test = df_test.loc[:, feats_to_keep].values
             
+        print("Retained %i features"%len(feats_to_keep))
+        X_train = df_train.loc[:,feats_to_keep].values
+        X_test = df_test.loc[:, feats_to_keep].values    
+        
         feats_used.append(feats_to_keep)
         X_tests.append(X_test)
         y_tests.append(y_test)
@@ -222,10 +235,12 @@ def cv_train_test(Dataset,
         stop3 = timeit.default_timer()
         print('Feature selection complete, ready for training, elapsed time: %.1f sec'%(stop3 - start))
         
-        model, scaler, y_pred = train_test(X_train, X_test, y_train, penalty=penalty, internalCV_folds=internalCV_folds)
+        model, scaler, y_pred, y_pred_prob = train_test(X_train, X_test, y_train, penalty=penalty, internalCV_folds=internalCV_folds)
         models.append(model)
+        scalers.append(scaler)
         y_preds.append(list(y_pred))
-        pred_probs.append(model.predict_proba(X_test)[:,1])
+        pred_probs.append(list(y_pred_prob))
+        # pred_probs.append(model.predict_proba(X_test)[:,1])
 
         stop4 = timeit.default_timer()
         print('Fold complete, fold time: %.1f sec'%(stop4 - start))
@@ -236,48 +251,50 @@ def cv_train_test(Dataset,
 
     timestamp = datetime.now().strftime("%m/%d/%Y, %H:%M:%S") # Save time models finished training
     run_params = {"penalty":penalty, "CV":CV, "internalCV_folds":internalCV_folds, "feat_selection":feat_selection, "nan_policy":nan_policy, "timestamp":timestamp} # save in output for reference
-    outputs = {"trained_models":models, "features_used":feats_used, "X_test_data":X_tests, "y_test":y_tests, "y_pred":y_pred, "y_pred_prob":pred_probs, "run_params":run_params} 
+    outputs = {"trained_models":models, "scalers":scalers, "features_used":feats_used, "X_test_data":X_tests, "y_test":y_tests, "y_pred":y_pred, "y_pred_prob":pred_probs, "run_params":run_params} 
     print("----CV complete----")
     return (outputs)
 
 
-def model_definition(penalty, seed=42, internalCV_folds=5, n_jobs=-1, Cs=None, l1_ratios=None):
+def model_definition(penalty, seed=42, internalCV_folds=5, n_jobs=-1, Cs=None, l1_ratios=None, max_iter=1000, class_weight='balanced'):  #TODO -FIX DEFAULTS BELOW *********************************
     """ create Logistic regression model object"""
     if Cs is None:
-        Cs=np.logspace(-4, 6, num=10) # default values of C to try out in CV (lower=stronger regularization)
+        l1_Cs=np.logspace(-4, 6, num=10) # default values of C to try out in CV (lower=stronger regularization)
+        l2_Cs=10
+    else:
+        l1_Cs=Cs
+        l2_Cs=Cs
+        
     if l1_ratios is None: # default values of l1 ratios to try out in CV for Elastic Net
         l1_ratios=[0.1,0.5,0.9]
         
-    if penalty=='l1':
-        model = LogisticRegressionCV(cv=internalCV_folds, random_state=seed, penalty='l1', solver='liblinear',
-                                     Cs=Cs, n_jobs=n_jobs)
+    if  penalty is None:
+        print("Not applying regularization")
+        model = LogisticRegressionCV(cv=internalCV_folds, random_state=seed, penalty='l2', Cs=[100000.0], max_iter=max_iter, class_weight=class_weight) #TODO - CHANGE TO NO REGULAROZATION AT ALL!!!!*********************************
+    elif penalty=='l1':
+        model = LogisticRegressionCV(cv=internalCV_folds, random_state=seed, penalty='l1', solver='liblinear', Cs=l1_Cs, n_jobs=n_jobs, max_iter=max_iter, class_weight=class_weight)
     elif penalty=='l2':
-        model = LogisticRegressionCV(cv=internalCV_folds, random_state=seed, penalty='l2', 
-                                     Cs=Cs)
+        model = LogisticRegressionCV(cv=internalCV_folds, random_state=seed, penalty='l2', Cs=l2_Cs, max_iter=max_iter, class_weight=class_weight)
     elif penalty=='elasticnet':
-        model = LogisticRegressionCV(cv=internalCV_folds, random_state=seed, penalty='elasticnet', solver='saga',
-                                     Cs=Cs, l1_ratios=l1_ratios)    
+        model = LogisticRegressionCV(cv=internalCV_folds, random_state=seed, penalty='elasticnet', solver='saga', l1_ratios=l1_ratios, class_weight=class_weight, max_iter=max_iter) # Cs=l2_Cs,#TODO - FIX DEFAULT Cs*****************  
     else:
         print("unrecognized penalty")
+    # print(model)
     return(model)
 
 
 def scale_train_data(X_train):
     # Standardizing the features (generally by train set only). 
     # Returns scaled train set data, and the scaler oobject itself for scaling test data later
-    scaler = StandardScaler().fit(X_train)
-    X_train = scaler.transform(X_train)    
-    return(X_train, scaler)
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    X_train_scaled = scaler.transform(X_train)    
+    return(X_train_scaled, scaler)
 
 
 def train_test(X_train, X_test, y_train, penalty = 'l1', seed=42, internalCV_folds=5, n_jobs=-1):
     """ Train model and get predictions """
-    # Standardizing the features (by train set only)
-
-    # scaler = StandardScaler().fit(X_train)
-    # X_train = scaler.transform(X_train)
-    # X_test = scaler.transform(X_test)
-    
+    # Standardizing the features (by train set only)  
     X_train, scaler = scale_train_data(X_train)
     X_test = scaler.transform(X_test)
 
@@ -285,7 +302,8 @@ def train_test(X_train, X_test, y_train, penalty = 'l1', seed=42, internalCV_fol
     model = model_definition(penalty=penalty, seed=seed, internalCV_folds=internalCV_folds, n_jobs=n_jobs, Cs=None, l1_ratios=None)    
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
-    return(model, scaler, y_pred)
+    y_pred_prob = model.predict_proba(X_test)[:,1]
+    return(model, scaler, y_pred, y_pred_prob)
 
 
 def train_model(X, y, penalty='l1', seed=42, internalCV_folds=5, n_jobs=-1):
@@ -333,7 +351,7 @@ def numba_fillna(array, values):
 
 # Data/Result vizualization tools:
 # ================================
-def plot_cv_roc(cv_res, plot_individual_folds=True, out_f=None):
+def plot_cv_roc(cv_res, plot_individual_folds=True, title_pfx="", out_f=None):
     fprs, tprs, aucs = [], [], []
     base_fpr = np.linspace(0, 1, 101)
     plt.figure()
@@ -348,7 +366,11 @@ def plot_cv_roc(cv_res, plot_individual_folds=True, out_f=None):
             tpr[0] = 0.0
             tprs.append(tpr)
         except:
-            continue
+            print("Some CV folds could not be plotted (missing labels). Trying plot_cv_single_roc() instead.")
+            plt.close()
+            plot_cv_single_roc(cv_res, title_pfx=title_pfx, out_f=out_f)
+            return 
+            # continue
     
     tprs = np.array(tprs)
     mean_tprs = tprs.mean(axis=0)
@@ -364,20 +386,39 @@ def plot_cv_roc(cv_res, plot_individual_folds=True, out_f=None):
     plt.ylim([-0.01, 1.01])
     plt.ylabel('True Positive Rate')
     plt.xlabel('False Positive Rate')
-    plt.title('Receiver operating characteristic')
+    plt.title(title_pfx + ' Receiver operating characteristic')
+    plt.legend(loc="lower right")
+    if out_f is not None:
+        plt.savefig(out_f)
+    plt.show()
+    
+    
+def plot_cv_single_roc(cv_res, title_pfx="", out_f=None):
+    y_true = [item for sublist in cv_res["y_test"] for item in sublist]
+    y_probas = [item for sublist in cv_res["y_pred_prob"] for item in sublist]
+    auc = roc_auc_score(y_true, y_probas)
+    fpr, tpr, thresholds = roc_curve(y_true,  y_probas)
+    plt.plot(fpr,tpr, c='darkblue', label="AUC=%0.2f"%auc)
+    plt.plot([0, 1], [0, 1],'k--')
+    plt.xlim([-0.01, 1.01])
+    plt.ylim([-0.01, 1.01])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.title(title_pfx + ' Receiver operating characteristic')
     plt.legend(loc="lower right")
     if out_f is not None:
         plt.savefig(out_f)
     plt.show()
 
-
-def plot_pred_prob_by_labels(cv_res, out_f=None):
+def plot_pred_prob_by_labels(cv_res, title_pfx="", out_f=None):
     sns.stripplot(x=[item for sublist in cv_res["y_test"] for item in sublist], 
                   y=[item for sublist in cv_res["y_pred_prob"] for item in sublist], 
-                  size=5, 
+                  size=3, 
                   palette=["darkred","lightblue"])
     plt.xlabel("True label")
     plt.ylabel("Model prediction\n(prob. for class 1)")
+    plt.title(title_pfx + ' Predicted probabilities per class')
+
     if out_f is not None:
         plt.savefig(out_f)        
     plt.show()
